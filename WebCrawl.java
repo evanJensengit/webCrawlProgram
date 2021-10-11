9816581360c78816d909791170f042f6e0069a6b
Evan Jensen <evanjensen@Evans-MBP.home>
2021-10-10 19:53:03 -0700

    no testing yet

--- WebCrawl.java
+++ WebCrawl.java
@@ -6,7 +6,55 @@ import java.net.URL;
 
 
 public class WebCrawl {
+    //checks url connection returns false if status code is >= 300 true otherwise
+    public static boolean checkURLCode ( HttpURLConnection connection) {
+       int statusCode = connection.getResponseCode();
 
+       if (statusCode >= 300) {
+           return false;
+       }
+       return true;
+   }
+   //finds the href in html body of url passed to method by creating BufferedReader
+    //if href is found then create connection, checks code and calls itself
+   public static int findHrefInURL ( HttpURLConnection connection, int urlHopsLeft) {
+       if (urlHopsLeft == 0) {
+           return 0;
+       }
+       BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+       String line;
+       String substring;
+       int locationOfHref;
+       String nextURL;
+       String delims = " \" ";
+       while ((line = reader.readLine()) != null){
+           locationOfHref = line.indexOf("href");
+           substring = line.substring(locationOfHref);
+           List <String> tokens = substring.split(delims);
+           nextURL = tokens[0];
+           //check if string starts with http
+           HttpURLConnection nextURLConnection = createConnection(nextURL);
+           boolean codeOk = checkURLCode(nextURLConnection);
+
+           if (codeOk) {
+               urlHopsLeft = findHrefInURL(nextURLConnection, urlHopsLeft-1 );
+
+               if (urlHopsLeft == 0) {
+                   return 0;
+               }
+           }
+
+       }
+   }
+
+   public static HttpURLConnection createConnection (String strURL) {
+       URL url = new URL(strURL); //make url object from string got from console input
+       HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+       return connection;
+   }
+   public static
     public static void main(String[] args) throws IOException  {
 
         if (args.length == 0 || args.length < 2) {
@@ -15,16 +63,16 @@ public class WebCrawl {
         }
         String strURL = args[0];
         int numberURLHops = Integer.parseInt(args[1]);
-        URL url = new URL(strURL); //make url object from string got from console input
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //open connection to url that was input
-        //in console
-        int statusCode = connection.getResponseCode();
+        HttpURLConnection connection = createConnection(strURL, numberURLHops)
+
+        boolean codeOk = checkURLCode(connection);
         System.out.println(statusCode);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = reader.readLine()) != null){
-            System.out.println(line);
+        if (!codeOk) {
+            System.out.println("Please enter valid url");
+            System.exit(0);
         }
+        findHrefInURL(connection, numberURLHops);
+        
     }
 }
